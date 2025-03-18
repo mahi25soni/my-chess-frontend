87e3ac881de10d3b884094865fb682e588d0b3ce
mahi25soni <msdraj02@protonmail.com>
2025-03-18 23:32:05 +0530

    feat(setup) : Basic tailwind setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,6 @@
 
 import React from "react";
 
-export default function Home({ name, className }: { name: string; className: string }) {
-  const message = "Hello World";
-  return (
-    <div className="bg-red-500">
-      mahendra
-      <img src="image.jpg" />
-      <button onClick={() => alert("Clicked!")}>Click Me</button>
-      <input type="text"></input>
-      <p>{"Use single quotes here"}</p>
-      <div>Click me</div> {/* Bad inline function */}
-    </div>
-  );
+export default function Home() {
+  return <div className="bg-red-500">mahendra</div>;
 }
