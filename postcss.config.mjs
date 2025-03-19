5cf753a780dab88af366a32171ed802bc1e40e36
mahi25soni <msdraj02@protonmail.com>
2025-03-19 13:57:59 +0530

    refactor(style) : new tailwind setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
-  theme: {
-    extend: {
-      colors: {
-        "blue": "#2563EB",
-        "green": "#166534",
-        "light-green": "#DCFCE7",
-        "dark-red": "#991B1B",
-        "dark-white": "#F3F4F6",
-        "off-white": "#4B5563",
-      },
-      borderColor: {
-        basic: "E5E7EB",
-      },
-    },
-  },
 };
+export default config;
